45463bc9c4dc77655030e1e400a25f00d851f90d
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-07-04 20:34:06 +0300

    feat: add project section

--- core/src/components/home-page/projectsCard.tsx
+++ core/src/components/home-page/projectsCard.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from "next/image";
 
-export default function ProjectsCard() {
+export default function ProjectsCard({url}: {url: string}) {
   return (
-    <div className='w-[280px] h-[366px] bg-primary04 rounded-[20px] mt-4 mb-4'>
+    <div className='w-[280px] h-[366px] bg-primary04 rounded-[20px] mt-4 mb-4 overflow-hidden'>
+       <Image
+            className="rounded-[20px]"
+            src={url}
+            width={280}
+            height={300}
+            alt="icon pattern"
+          />
     </div>
   )
 }
--- core/src/components/home-page/projectsCards.tsx
+++ core/src/components/home-page/projectsCards.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 import ProjectsCard from './projectsCard'
 
 const dataProject = [
-    {id: 1, title: '', description: '', url: '', image: ''},
-    {id: 2, title: '', description: '', url: '', image: ''},
-    {id: 3, title: '', description: '', url: '', image: ''},
-    {id: 4, title: '', description: '', url: '', image: ''},
-    {id: 5, title: '', description: '', url: '', image: ''},
-    {id: 6, title: '', description: '', url: '', image: ''},
-    {id: 7, title: '', description: '', url: '', image: ''},
-    {id: 8, title: '', description: '', url: '', image: ''},
+    {id: 1, title: '', description: '', url: '/projects/1.jpg', image: ''},
+    {id: 2, title: '', description: '', url: '/projects/2.jpg', image: ''},
+    {id: 3, title: '', description: '', url: '/projects/3.jpg', image: ''},
+    {id: 4, title: '', description: '', url: '/projects/4.jpg', image: ''},
+    {id: 5, title: '', description: '', url: '/projects/5.jpg', image: ''},
+    {id: 6, title: '', description: '', url: '/projects/6.jpg', image: ''},
+    {id: 7, title: '', description: '', url: '/projects/7.jpg', image: ''},
+    {id: 8, title: '', description: '', url: '/projects/8.jpg', image: ''},
 ]
 
 export default function ProjectsCards() {
   return (
     <div className='flex mt-[20px] justify-between flex-wrap'>
-    {dataProject.map((el) => <ProjectsCard  key={el.id}/>)}
+    {dataProject.map((el) => <ProjectsCard url={el.url}  key={el.id}/>)}
     </div>
   )
 }
