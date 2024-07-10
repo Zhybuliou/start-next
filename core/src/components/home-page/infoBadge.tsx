d261993f6a71c66042781cccc2d57483d1486b34
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-07-10 14:45:33 +0300

    feat: add z-index

--- core/src/components/home-page/infoBadge.tsx
+++ core/src/components/home-page/infoBadge.tsx
@@ -5,9 +5,9 @@ import { PiUsersThree } from "react-icons/pi";
 export default function InfoBadge({ className }: { className: string }) {
   return (
     <div className={`${className}`}>
-      <div className="absolute w-[150px] h-[150px] bg-primary02 rounded-full"></div>
-      <div className="absolute right-0 bottom-0 w-[150px] h-[150px] bg-primary01 rounded-full"></div>
-      <div className="max-w-[343px] w-[100%] h-[163px] backdrop-blur-md border-2 rounded-[25px] m-auto mt-8 flex justify-center items-center">
+      <div className="absolute w-[150px] h-[150px] bg-primary02 rounded-full z-0"></div>
+      <div className="absolute right-0 bottom-0 w-[150px] h-[150px] bg-primary01 rounded-full z-0"></div>
+      <div className="max-w-[343px] w-[100%] h-[163px] backdrop-blur-md border-2 rounded-[25px] m-auto mt-8 flex justify-center items-center z-20">
         <PiUsersThree className="text-[110px] mr-4" />
         <div className="w-[150px]">
           <Counter
