c1f2a8d01394cf8d6dc6463816143d9eb7164b51
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-07-10 15:04:35 +0300

    feat: add style infoBadge

--- core/src/components/home-page/infoBadge.tsx
+++ core/src/components/home-page/infoBadge.tsx
@@ -5,6 +5,7 @@ import { PiUsersThree } from "react-icons/pi";
 export default function InfoBadge({ className }: { className: string }) {
   return (
     <div className={`${className}`}>
+      <div className="relative h-[100%] w-[100%]">
       <div className="absolute w-[150px] h-[150px] bg-primary02 rounded-full z-0"></div>
       <div className="absolute right-0 bottom-0 w-[150px] h-[150px] bg-primary01 rounded-full z-0"></div>
       <div className="max-w-[343px] w-[100%] h-[163px] backdrop-blur-md border-2 rounded-[25px] m-auto mt-8 flex justify-center items-center z-20">
@@ -18,6 +19,7 @@ export default function InfoBadge({ className }: { className: string }) {
           />
           <p className="text-paragraph">Satisfied Clients</p>
         </div>
+      </div>
       </div>
     </div>
   );
