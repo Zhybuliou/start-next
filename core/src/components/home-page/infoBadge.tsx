e6ce9cfa0fcd38efc821d284c51852f9103afb71
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-07-10 14:33:12 +0300

    feat: add width for counter

--- core/src/components/home-page/infoBadge.tsx
+++ core/src/components/home-page/infoBadge.tsx
@@ -9,7 +9,7 @@ export default function InfoBadge({ className }: { className: string }) {
       <div className="absolute right-0 bottom-0 w-[150px] h-[150px] bg-primary01 rounded-full"></div>
       <div className="max-w-[343px] w-[100%] h-[163px] backdrop-blur-md border-2 rounded-[25px] m-auto mt-8 flex justify-center items-center">
         <PiUsersThree className="text-[110px] mr-4" />
-        <div>
+        <div className="w-[150px]">
           <Counter
             start={0}
             end={1500}
